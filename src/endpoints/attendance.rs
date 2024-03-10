e933db7b26c5b527562ea9d5bd504d9c56ca17b5
Killian O'Gorman <83577130+killianogorman@users.noreply.github.com>
2024-03-10 11:40:00 +0000

    add func get_half_day_summary_grid_lines and types

--- src/endpoints/attendance.rs
+++ src/endpoints/attendance.rs
@@ -213,6 +213,204 @@ pub struct AttendanceSummary {
 struct GetAttendanceSumRes {
     d: Vec<AttendanceSumRes>,
 }
+async fn get_half_day_summary_grid_lines(
+    cookies: String,
+    year: i32,
+    user_id: i32,
+    school_id: &str,
+) -> Result<String, reqwest::Error> {
+    let url = format!(
+        "https://{}.compass.education/Services/Attendance.svc/GetAttendanceSummary",
+        school_id
+    );
+    let mut headers = reqwest::header::HeaderMap::new();
+    let cookies_str: &str = cookies.as_str();
+    headers.insert(
+        "cookie",
+        reqwest::header::HeaderValue::from_str(&cookies_str).unwrap(),
+    );
+    let req = reqwest::ClientBuilder::new()
+        .cookie_store(true)
+        .default_headers(headers)
+        .user_agent(USER_AGENT)
+        .build()?
+        .post(url)
+        .json(&GetHalfDaySummaryGridLinesReq {
+            user_id,
+            year,
+            page: 0,
+            start: 0,
+            limit: 1000,
+        })
+        .send()
+        .await?;
+    let res = req.json::<GetHalfDaySummaryGridLinesRes>().await?;
+    let mut get_half_day_summary_grid_lines: Vec<SummaryGridLine> = Vec::new();
+    for i in res.d {
+        let v = SummaryGridLine {
+            am_extended_status_id: i.am_extended_status_id,
+            am_status: i.am_status,
+            am_status_desc: i.am_status_desc,
+            am_status_export_identifier: i.am_status_export_identifier,
+            am_status_period_calc_not_marked: i.am_status_period_calc_not_marked,
+            counted_absence: i.counted_absence,
+            counted_absence_export_identifier: i.counted_absence_export_identifier,
+            counted_absence_reason_desc: i.counted_absence_reason_desc,
+            counted_absence_reason_status: i.counted_absence_reason_status,
+            date: i.date,
+            date_equality_property: i.date_equality_property,
+            day_of_week: i.day_of_week,
+            most_prevalent_desc: i.most_prevalent_desc,
+            most_prevalent_export_identifier: i.most_prevalent_export_identifier,
+            most_prevalent_status: i.most_prevalent_status,
+            partial_absence: i.partial_absence,
+            partial_day_export_identifier: i.partial_day_export_identifier,
+            pm_extended_status_id: i.pm_extended_status_id,
+            pm_status: i.pm_status,
+            pm_status_desc: i.pm_status_desc,
+            pm_status_export_identifier: i.pm_status_export_identifier,
+            pm_status_period_calc_not_marked: i.pm_status_period_calc_not_marked,
+            qld_half_day_code_am: i.qld_half_day_code_am,
+            qld_half_day_code_pm: i.qld_half_day_code_pm,
+            time_amount_absent: i.time_amount_absent,
+            time_amount_absent_counted: i.time_amount_absent_counted,
+            time_amount_absent_not_counted: i.time_amount_absent_not_counted,
+            time_amount_arrived_late_am: i.time_amount_arrived_late_am,
+            time_amount_arrived_late_pm: i.time_amount_arrived_late_pm,
+            time_amount_expected: i.time_amount_expected,
+            time_amount_not_marked: i.time_amount_not_marked,
+            time_amount_unscheduled: i.time_amount_unscheduled,
+            user_id,
+            whole_day_absence: i.whole_day_absence,
+            whole_day_export_identifier: i.whole_day_export_identifier,
+        };
+        get_half_day_summary_grid_lines.push(v)
+    }
+    Ok("()".to_string())
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct GetHalfDaySummaryGridLinesReq {
+    #[serde(rename = "userId")]
+    user_id: i32,
+    year: i32,
+    page: i32,
+    start: i32,
+    limit: i32,
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct GetHalfDaySummaryGridLinesRes {
+    d: Vec<SummaryGridLineRes>,
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct SummaryGridLineRes {
+    #[serde(rename = "amExtendedStatusId")]
+    am_extended_status_id: i64,
+    #[serde(rename = "amStatus")]
+    am_status: i16,
+    #[serde(rename = "amStatusDesc")]
+    am_status_desc: String,
+    #[serde(rename = "amStatusExportIdentifier")]
+    am_status_export_identifier: String,
+    #[serde(rename = "amStatusPeriodCalcNotMarked")]
+    am_status_period_calc_not_marked: bool,
+    #[serde(rename = "countedAbsence")]
+    counted_absence: bool,
+    #[serde(rename = "countedAbsenceExportIdentifier")]
+    counted_absence_export_identifier: Option<String>,
+    #[serde(rename = "countedAbsenceReasonDesc")]
+    counted_absence_reason_desc: String,
+    #[serde(rename = "countedAbsenceReasonStatus")]
+    counted_absence_reason_status: Option<i32>,
+    date: String,
+    #[serde(rename = "dateEqualityProperty")]
+    date_equality_property: String,
+    #[serde(rename = "dayOfWeek")]
+    day_of_week: i8,
+    #[serde(rename = "mostPrevalentDesc")]
+    most_prevalent_desc: String,
+    #[serde(rename = "mostPrevalentExportIdentifier")]
+    most_prevalent_export_identifier: Option<String>,
+    #[serde(rename = "mostPrevalentStatus")]
+    most_prevalent_status: Option<i16>,
+    #[serde(rename = "partialAbsence")]
+    partial_absence: bool,
+    #[serde(rename = "partialDayExportIdentifier")]
+    partial_day_export_identifier: String,
+    #[serde(rename = "pmExtendedStatusId")]
+    pm_extended_status_id: i32,
+    #[serde(rename = "pmStatus")]
+    pm_status: i16,
+    #[serde(rename = "pmStatusDesc")]
+    pm_status_desc: String,
+    #[serde(rename = "pmStatusExportIdentifier")]
+    pm_status_export_identifier: String,
+    #[serde(rename = "pmStatusPeriodCalcNotMarked")]
+    pm_status_period_calc_not_marked: bool,
+    #[serde(rename = "qldHalfDayCodeAM")]
+    qld_half_day_code_am: i8,
+    #[serde(rename = "qldHalfDayCodePM")]
+    qld_half_day_code_pm: i8,
+    #[serde(rename = "timeAmountAbsent")]
+    time_amount_absent: i64,
+    #[serde(rename = "timeAmountAbsentCounted")]
+    time_amount_absent_counted: i64,
+    #[serde(rename = "timeAmountAbsentNotCounted")]
+    time_amount_absent_not_counted: i64,
+    #[serde(rename = "timeAmountArrivedLateAm")]
+    time_amount_arrived_late_am: Option<String>,
+    #[serde(rename = "timeAmountArrivedLatePm")]
+    time_amount_arrived_late_pm: Option<String>,
+    #[serde(rename = "timeAmountExpected")]
+    time_amount_expected: i64,
+    #[serde(rename = "timeAmountNotMarked")]
+    time_amount_not_marked: i64,
+    #[serde(rename = "timeAmountUnscheduled")]
+    time_amount_unscheduled: i64,
+    #[serde(rename = "userId")]
+    user_id: i32,
+    #[serde(rename = "wholeDayAbsence")]
+    whole_day_absence: bool,
+    #[serde(rename = "wholeDayExportIdentifier")]
+    whole_day_export_identifier: String,
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SummaryGridLine {
+    pub am_extended_status_id: i64,
+    pub am_status: i16,
+    pub am_status_desc: String,
+    pub am_status_export_identifier: String,
+    pub am_status_period_calc_not_marked: bool,
+    pub counted_absence: bool,
+    pub counted_absence_export_identifier: Option<String>,
+    pub counted_absence_reason_desc: String,
+    pub counted_absence_reason_status: Option<i32>,
+    pub date: String,
+    pub date_equality_property: String,
+    pub day_of_week: i8,
+    pub most_prevalent_desc: String,
+    pub most_prevalent_export_identifier: Option<String>,
+    pub most_prevalent_status: Option<i16>,
+    pub partial_absence: bool,
+    pub partial_day_export_identifier: String,
+    pub pm_extended_status_id: i32,
+    pub pm_status: i16,
+    pub pm_status_desc: String,
+    pub pm_status_export_identifier: String,
+    pub pm_status_period_calc_not_marked: bool,
+    pub qld_half_day_code_am: i8,
+    pub qld_half_day_code_pm: i8,
+    pub time_amount_absent: i64,
+    pub time_amount_absent_counted: i64,
+    pub time_amount_absent_not_counted: i64,
+    pub time_amount_arrived_late_am: Option<String>,
+    pub time_amount_arrived_late_pm: Option<String>,
+    pub time_amount_expected: i64,
+    pub time_amount_not_marked: i64,
+    pub time_amount_unscheduled: i64,
+    pub user_id: i32,
+    pub whole_day_absence: bool,
+    pub whole_day_export_identifier: String,
+}
 // pub async fn get_periods_for_timeline(
 //     cookies: String,
 //     date: String,
